7bee48adcf901d6f8103d8221431aeae27cdbe27
Ashish Gaikwad <gaikwadashish2477@gmail.com>
2026-04-30 08:43:09 +0000

    Add initial setup for frontend with React and Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>TreadPe - A full-stack stock trading platform built with MERN Stack</h1>
+    </>
+  )
+}
+
+export default App
