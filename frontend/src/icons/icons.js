184bde182309c3422d5ec793eca92f0e62ef9a6e
Ashish Gaikwad <gaikwadashish2477@gmail.com>
2026-05-01 06:16:13 +0000

    chore: replace Bootstrap and Font Awesome CDN links with npm packages for Bootstrap and react-icons

---
+++ frontend/src/icons/icons.js
@@ -0,0 +1,4 @@
+export { FaChartLine, FaArrowTrendUp, FaArrowTrendDown } from 'react-icons/fa6';
+export { MdDashboard } from 'react-icons/md';
+export { BiWallet } from 'react-icons/bi';
+export { FaBell, FaGear } from 'react-icons/fa6';
